7d7a2a7e35def956119ce3a377445353d730503f
Eivind Uggedal <eivind@uggedal.com>
2022-12-28 22:08:51 +0100

    null-ls: only enable mypy when using in project

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -4,7 +4,11 @@ return {
       null_ls.builtins.formatting.black,
       null_ls.builtins.formatting.ruff,
       null_ls.builtins.diagnostics.ruff,
-      null_ls.builtins.diagnostics.mypy,
+      null_ls.builtins.diagnostics.mypy.with({
+        condition = function(utils)
+          return utils.root_has_file('venv/bin/mypy')
+        end,
+      }),
 
       null_ls.builtins.formatting.stylua.with({
         extra_args = {
