e2cfca944a2c408df7c061cd3786cd2ca7214a45
Eivind Uggedal <eivind@uggedal.com>
2022-12-31 11:49:51 +0100

    lazy: fix change_detection disabling

--- init.lua
+++ init.lua
@@ -19,7 +19,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup('plugins', {
   defaults = { lazy = true },
   install = { missing = false },
-  change_detection = { enabled = disable },
+  change_detection = { enabled = false },
   rtp = {
     disabled_plugins = {
       'gzip',
