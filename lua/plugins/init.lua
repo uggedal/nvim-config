0f11189b1736667bb6073a89549e370686570257
Eivind Uggedal <eivind@uggedal.com>
2022-12-31 11:26:23 +0100

    plugins: remove packer comments

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,26 +2,3 @@ return {
   { 'ethanholz/nvim-lastplace', event = 'BufReadPre', config = {} },
   { 'jghauser/mkdir.nvim', event = 'BufWritePre' },
 }
-
--- req('packer', function(packer)
---   packer.startup({
---     function(use)
---
---       use('neovim/nvim-lspconfig')
---       use({
---         'jose-elias-alvarez/null-ls.nvim',
---         requires = { 'nvim-lua/plenary.nvim' },
---       })
---
---       use('hrsh7th/nvim-cmp')
---       use('hrsh7th/cmp-buffer')
---       use('hrsh7th/cmp-path')
---       use('hrsh7th/cmp-nvim-lsp')
---     end,
---   })
--- end)
---
--- req('lspconfig')
--- req('null-ls')
--- req('cmp')
---
