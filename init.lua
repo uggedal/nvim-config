64664f5c8f621c5644bddb96a1c200e0df0176bb
Eivind Uggedal <eivind@uggedal.com>
2023-01-01 12:14:46 +0100

    lazy: stable branch

--- init.lua
+++ init.lua
@@ -9,11 +9,12 @@ if not vim.loop.fs_stat(lazypath) then
     'git',
     'clone',
     '--filter=blob:none',
-    '--single-branch',
-    'git@github.com:folke/lazy.nvim.git',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
     lazypath,
   })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup('plugins', {
