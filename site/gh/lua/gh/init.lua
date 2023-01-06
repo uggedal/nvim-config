b44fbd9c17f292aa3e6572a2bb17718d5bab011c
Eivind Uggedal <eivind@uggedal.com>
2023-01-06 09:57:39 +0100

    gh: remove commented out old highlights

--- site/gh/lua/gh/init.lua
+++ site/gh/lua/gh/init.lua
@@ -18,27 +18,6 @@ hl(require('gh.editor'))
 hl(require('gh.syntax'))
 hl(require('gh.plugins'))
 
---
--- TODO: LSP (should probably use non-Lsp prefixed variants)
---
-
--- li('LspReferenceText', 'CursorLine')
--- li('LspReferenceRead', 'CursorLine')
--- li('LspReferenceWrite', 'CursorLine')
-
--- hl('LspDiagnosticsDefaultError', c.red_1)
--- hl('LspDiagnosticsUnderlineError', nil, nil, 'underline', c.red_1)
--- hl('LspDiagnosticsVirtualTextError', c.red_1, c.red_3)
--- hl('LspDiagnosticsDefaultWarning', c.orange_1)
--- hl('LspDiagnosticsUnderlineWarning', nil, nil, 'underline', c.orange_1)
--- hl('LspDiagnosticsVirtualTextWarning', c.orange_1, c.yellow_3)
--- hl('LspDiagnosticsDefaultInformation', c.blue_1)
--- hl('LspDiagnosticsUnderlineInformation', nil, nil, 'underline', c.blue_1)
--- hl('LspDiagnosticsVirtualTextInformation', c.blue_1, c.blue_3)
--- hl('LspDiagnosticsDefaultHint', c.green_1)
--- hl('LspDiagnosticsUnderlineHint', nil, nil, 'underline', c.green_1)
--- hl('LspDiagnosticsVirtualTextHint', c.green_1, c.green_3)
-
 -- Highlight trailing space when not in insert mode:
 local highlight_trailing = vim.api.nvim_create_augroup('HighlightTrailing', {})
 
