5e07ce2db8b2414e9725f409d0cc45bca8898cef
Eivind Uggedal <eivind@uggedal.com>
2025-04-08 12:12:47 +0200

    mini.deps: start transition from lazy

--- init.lua
+++ init.lua
@@ -1,45 +1,74 @@
-require('settings')
-require('languages')
-require('keymaps')
+-- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
+local path_package = vim.fn.stdpath('data') .. '/site/'
+local mini_path = path_package .. 'pack/deps/start/mini.nvim'
 
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
-  })
+if not vim.loop.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = { 'git', 'clone', '--filter=blob:none', 'https://github.com/echasnovski/mini.nvim', mini_path }
+  vim.fn.system(clone_cmd)
+  vim.cmd('packadd mini.nvim | helptags ALL')
+  vim.cmd('echo "Installed `mini.nvim`" | redraw')
 end
 
-vim.opt.rtp:prepend(lazypath)
+-- Set up 'mini.deps' (customize to your liking)
+require('mini.deps').setup({ path = { package = path_package } })
 
-require('lazy').setup('plugins', {
-  defaults = { lazy = true },
-  install = { missing = false },
-  change_detection = { enabled = false },
-  rtp = {
-    disabled_plugins = {
-      'gzip',
-      'matchit',
-      'matchparen',
-      'netrwPlugin',
-      'tarPlugin',
-      'tohtml',
-      'tutor',
-      'zipPlugin',
-    },
-  },
-  dev = {
-    path = '~/.config/nvim/site',
-  },
-  ui = {
-    border = 'rounded',
-  },
-})
+-- Use 'mini.deps'. `now()` and `later()` are helpers for a safe two-stage
+-- startup and are optional.
+local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 
-vim.o.background = 'light'
-vim.cmd('colorscheme gh')
+-- Safely execute immediately
+now(function()
+  require('gh')
+  vim.o.background = 'light'
+  vim.cmd('colorscheme gh')
+end)
+now(function()
+  require('settings')
+  require('languages')
+  require('keymaps')
+end)
+
+now(function()
+  require('mini.tabline').setup({
+    show_icons = false,
+  })
+end)
+
+-- Safely execute later
+-- later(function() require('mini.ai').setup() end)
+-- later(function() require('mini.comment').setup() end)
+-- later(function() require('mini.pick').setup() end)
+-- later(function() require('mini.surround').setup() end)
+
+now(function()
+  -- Use other plugins with `add()`. It ensures plugin is available in current
+  -- session (installs if absent)
+  -- add({
+  --   source = 'neovim/nvim-lspconfig',
+  --   -- Supply dependencies near target plugin
+  --   depends = { 'williamboman/mason.nvim' },
+  -- })
+  add({
+    source = 'ethanholz/nvim-lastplace'
+  })
+end)
+
+later(function()
+  add({
+    source = 'jghauser/mkdir.nvim'
+  })
+  -- add({
+  --   source = 'nvim-treesitter/nvim-treesitter',
+  --   -- Use 'master' while monitoring updates in 'main'
+  --   checkout = 'master',
+  --   monitor = 'main',
+  --   -- Perform action after every checkout
+  --   hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
+  -- })
+  -- Possible to immediately execute code which depends on the added plugin
+  -- require('nvim-treesitter.configs').setup({
+  --   ensure_installed = { 'lua', 'vimdoc' },
+  --   highlight = { enable = true },
+  -- })
+end)
--- lua/plugins/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-  { 'ethanholz/nvim-lastplace', event = 'BufReadPre', config = {} },
-  { 'jghauser/mkdir.nvim', event = 'BufWritePre' },
-  {
-    'uggedal/gh',
-    priority = 1000,
-    dev = true,
-  },
-}
