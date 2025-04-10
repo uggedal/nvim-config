dbe0b631edabbdc0614b54b5da5839dc60d33df2
Eivind Uggedal <eivind@uggedal.com>
2025-04-10 09:31:00 +0200

    completion: drop cmp and use omni with tab mapping

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -49,3 +49,33 @@ map(
 )
 
 map('n', 'gl', vim.diagnostic.open_float, { desc = 'view diagnostic' })
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0
+    and vim.api
+        .nvim_buf_get_lines(0, line - 1, line, true)[1]
+        :sub(col, col)
+        :match('%s')
+      == nil
+end
+
+map('i', '<Tab>', function()
+  if vim.fn.pumvisible() == 1 then
+    return '<C-n>'
+  end
+
+  if has_words_before() then
+    return '<C-x><C-o>'
+  end
+
+  return '<Tab>'
+end, { expr = true, desc = 'indent or omni completion' })
+
+map('i', '<S-Tab>', function()
+  if vim.fn.pumvisible() == 1 then
+    return '<C-p>'
+  end
+
+  return '<S-Tab>'
+end, { expr = true, desc = 'previous match if completing' })
