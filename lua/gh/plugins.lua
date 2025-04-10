1e25ca1d022dda58e3025cd7a019970121bb211b
Eivind Uggedal <eivind@uggedal.com>
2025-04-10 10:31:51 +0200

    gh: drop colors for unused plugins

--- lua/gh/plugins.lua
+++ lua/gh/plugins.lua
@@ -8,17 +8,4 @@ return {
   GitSignsAddNr = { bg = c.green_bg },
   GitSignsChangeNr = { bg = c.yellow_bg_1 },
   GitSignsDeleteNr = { bg = c.red_bg },
-
-  -- Which Key
-  WhichKey = { fg = c.red_1, bold = true },
-  WhichKeyGroup = { fg = c.fg_1 },
-  WhichKeySeparator = { fg = c.fg_1 },
-  WhichKeyDesc = { fg = c.fg_1 },
-  WhichKeyFloat = { bg = c.bg_1 },
-
-  -- LSP Config
-  LspInfoBorder = { fg = c.bg_2 },
-
-  -- Null Ls
-  NullLsInfoBorder = { fg = c.bg_2 },
 }
